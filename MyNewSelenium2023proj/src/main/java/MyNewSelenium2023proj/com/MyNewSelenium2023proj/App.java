9fddd9b2b3a1d4739c21ebec2110c624d4f46eb1
gaan.amarendra <gaan.amarendra@gmail.com>
2023-01-15 12:46:32 +0530

    My first commit

---
+++ MyNewSelenium2023proj/src/main/java/MyNewSelenium2023proj/com/MyNewSelenium2023proj/App.java
@@ -0,0 +1,13 @@
+package MyNewSelenium2023proj.com.MyNewSelenium2023proj;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ MyNewSelenium2023proj/src/test/java/AppTestTest.java
@@ -0,0 +1,7 @@
+import org.testng.annotations.Test;
+
+public class AppTestTest {
+  @Test
+  public void f() {
+  }
+}
