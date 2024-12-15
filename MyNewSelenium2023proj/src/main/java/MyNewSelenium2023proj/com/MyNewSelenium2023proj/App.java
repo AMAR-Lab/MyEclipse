1e2392c48b94f3b7f34cbbbdb443fe09b9d668e1
Amar Kumar <gaan.amarendra@gmail.com>
2024-12-15 16:19:04 +0530

    3rd commits massage

--- MyNewSelenium2023proj/src/main/java/MyNewSelenium2023proj/com/MyNewSelenium2023proj/App.java
+++ MyNewSelenium2023proj/src/main/java/MyNewSelenium2023proj/com/MyNewSelenium2023proj/App.java
@@ -1,13 +1,30 @@
 package MyNewSelenium2023proj.com.MyNewSelenium2023proj;
 
-/**
- * Hello world!
- *
- */
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws InterruptedException
     {
         System.out.println( "Hello Amar 2024 can we start Automation" );
+        
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://www.amazon.in/");
+        driver.manage().window().maximize();
+        
+        driver.findElement(By.id("twotabsearchtextbox")).sendKeys("iphone 16 black 256gb");
+        
+        driver.findElement(By.id("nav-search-submit-button")).click();
+        
+        Thread.sleep(5000);
+        
+        
+        
+       // driver.quit();
+        
+        
     }
 }
